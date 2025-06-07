062ec001931a8a7f040f8dbd3016ad18d37d77aa
Camgobeg <camgobeg@gmail.com>
2025-06-07 09:09:36 +0300

    Update omni_P13_Blue_Max_128_GB.mk

--- omni_P13_Blue_Max_128_GB.mk
+++ omni_P13_Blue_Max_128_GB.mk
@@ -10,6 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from P13_Blue_Max_128_GB device
 $(call inherit-product, device/chinoe/P13_Blue_Max_128_GB/device.mk)
